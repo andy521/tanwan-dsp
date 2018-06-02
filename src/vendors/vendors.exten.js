abcd7411f46687cf90430d67da5027b0ea6b4327
q275957304 <275957304@qq.com>
2018-06-02 09:06:24 +0800

    登录默认菜单修改

--- src/vendors/vendors.exten.js
+++ src/vendors/vendors.exten.js
@@ -2,3 +2,5 @@ import axios from 'axios';
 import clipboard from 'clipboard';
 import html2canvas from 'html2canvas';
 import rasterizehtml from 'rasterizehtml';
+import echarts from 'echarts';
+import countup from 'countup';
